2d8edbcbfc25bd50b46111d148bc980d6e0cba51
Ian Kincaid <ikbusinessacct@gmail.com>
2019-12-03 22:57:35 -0500

    made the circle slots but need to add a class to the nested divs

--- script.js
+++ script.js
@@ -10,9 +10,38 @@ let col1 = function (){
   for(let i = 0; i < 42; i++){
     let newCol =  document.createElement(`div`);
     newCol.classList.add(`box`);
-    newCol.style.backgroundColor = `white`;
+    newCol.style.backgroundColor = `black`;
   let myBox =  document.querySelector('.box');
   myBox.append(newCol);
 }
 }
 col1();
+
+// let clickBox = document.querySelectorAll(".box")
+
+// let clickMe = function (){
+//   for(let i = 0; i < clickBox.length; i++){
+// function evt(){ clickBox.addEventListener('click', function(userClicked){
+//   userClicked.preventDefalut();
+//   if(userClicked.target.tagName === "DIV"){
+//     clickBox.style.backgroundColor = 'yellow';
+//   }
+  
+// })  
+// }
+// }}
+// clickMe();
+
+// let addClass = document.querySelectorAll("div > div");
+// let slots = function (){
+//   for(let i = 0; i < addClass.length; i++){
+//   let crClass = document.createAttributeNS('class','littleBoxs')
+//   crClass.value = "littleBoxs";
+//   function ent(){
+//   addClass.setAttribute(crClass);
+// }
+// }
+// }
+// slots();
+
+
